11afce95b94b7cefba38388a634a3984393714e8
RubasznyChomik <poreba.mateusz@gmail.com>
2022-05-05 22:20:47 +0200

    edited bugs

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 var counter = 0;
 var gameStarted = false;
+var sequenceDone = false;
+var btnReadyForClick = true;
 var canClick = false;
 var gameSequence = [];
 var playerSequenceNumber = 0;
 
-
-$(document).keypress(function () {
-    if (!gameStarted) {
+if (!gameStarted) {
+    $(document).keypress(function () {
         randomChooseNext();
         $("#level-title").text("Level: " + counter);
         canClick = true;
         gameStarted = true;
-        playerSequenceNumber = 0;
-        counter=0;
-        gameSequence = [];
-        
         while (canClick) {
             playerClick("red");
             playerClick("green");
@@ -22,74 +19,69 @@ $(document).keypress(function () {
             playerClick("blue");
             canClick = false;
         }
-    }
-});
-
-
-function playerClick(color) {
-    $("." + color).click(function () {
-        console.log(gameSequence[playerSequenceNumber]);
-        if (gameSequence[playerSequenceNumber] == color) {
-            playerSequenceNumber++;
-            canClick = true
-            var sound = new Audio("sounds/yellow.mp3");
-            sound.play();
-            if (playerSequenceNumber >= gameSequence.length) {
-                playerSequenceNumber = 0;
-                counter++;
-                randomChooseNext()
-            }
-        } else if(gameStarted){
-            var sound = new Audio("sounds/wrong.mp3");
-            sound.play();
-
-            $("body").addClass("game-over");
-            setTimeout(() => {
-                $("body").removeClass("game-over");
-            }, 600);
-
-            $("#level-title").text("Press A Key to Start");
-            gameStarted = false;
-        }
     });
 }
 
+function playerClick(color) {
+    setTimeout(() => {
+        $("." + color).click(function () {
+            if (gameSequence[playerSequenceNumber] == color) {
+                playerSequenceNumber++;
+                canClick=true
+                if (playerSequenceNumber == gameSequence.length) {
+                    playerSequenceNumber = 0;
+                    counter++;
+                    randomChooseNext()
+                }
+            } else {
+                $("body").addClass("game-over");
+                setTimeout(() => {
+                    $("body").removeClass("game-over");
+                }, 600);
+                counter = 0;
+                $("#level-title").text("Press A Key to Start");
+                gameStarted = false;
+                gameSequence = [];
+            }
+
+        });
+    }, 1000);
+}
 function randomChooseNext() {
     var btnNumber = Math.floor(Math.random() * 4);
 
     switch (btnNumber) {
         case 0:
             $(".red").addClass("pressed");
-            gameSequence[counter] = ("red");
             setTimeout(() => {
                 $(".red").removeClass("pressed");
             }, 400);
+            gameSequence[counter] = ("red");
             break;
         case 1:
             $(".green").addClass("pressed");
-            gameSequence[counter] = ("green");
             setTimeout(() => {
                 $(".green").removeClass("pressed");
             }, 400);
+            gameSequence[counter] = ("green");
             break;
         case 2:
             $(".yellow").addClass("pressed");
-            gameSequence[counter] = ("yellow");
             setTimeout(() => {
                 $(".yellow").removeClass("pressed");
             }, 400);
+            gameSequence[counter] = ("yellow");
             break;
         case 3:
             $(".blue").addClass("pressed");
-            gameSequence[counter] = ("blue");
             setTimeout(() => {
                 $(".blue").removeClass("pressed");
             }, 400);
+            gameSequence[counter] = ("blue");
             break;
         default:
             break;
     }
-    console.log(gameSequence[counter]);
 }
 
 
