1a50c45fdb24eee1337bdcfca9cece777420dcf1
RubasznyChomik <poreba.mateusz@gmail.com>
2022-05-05 23:26:04 +0200

    errors resolved

--- index.js
+++ index.js
@@ -7,39 +7,35 @@ var playerSequenceNumber = 0;
 
 $(document).keypress(function () {
     if (!gameStarted) {
+        gameSequence = [];
+        counter = 0;
         randomChooseNext();
         $("#level-title").text("Level: " + counter);
-        canClick = true;
         gameStarted = true;
         playerSequenceNumber = 0;
-        counter=0;
-        gameSequence = [];
-        
-        while (canClick) {
-            playerClick("red");
-            playerClick("green");
-            playerClick("yellow");
-            playerClick("blue");
-            canClick = false;
-        }
     }
 });
 
-
+playerClick("red");
+playerClick("green");
+playerClick("yellow");
+playerClick("blue");
 function playerClick(color) {
-    $("." + color).click(function () {
-        console.log(gameSequence[playerSequenceNumber]);
+
+    $("." + color).mousedown(function () {
+
         if (gameSequence[playerSequenceNumber] == color) {
             playerSequenceNumber++;
-            canClick = true
+
             var sound = new Audio("sounds/yellow.mp3");
             sound.play();
+            canClick = true
             if (playerSequenceNumber >= gameSequence.length) {
                 playerSequenceNumber = 0;
                 counter++;
                 randomChooseNext()
             }
-        } else if(gameStarted){
+        } else if (gameStarted) {
             var sound = new Audio("sounds/wrong.mp3");
             sound.play();
 
@@ -55,41 +51,44 @@ function playerClick(color) {
 }
 
 function randomChooseNext() {
-    var btnNumber = Math.floor(Math.random() * 4);
+    let btnNumber = Math.floor(Math.random() * 4);
 
-    switch (btnNumber) {
-        case 0:
-            $(".red").addClass("pressed");
-            gameSequence[counter] = ("red");
-            setTimeout(() => {
-                $(".red").removeClass("pressed");
-            }, 400);
-            break;
-        case 1:
-            $(".green").addClass("pressed");
-            gameSequence[counter] = ("green");
-            setTimeout(() => {
-                $(".green").removeClass("pressed");
-            }, 400);
-            break;
-        case 2:
-            $(".yellow").addClass("pressed");
-            gameSequence[counter] = ("yellow");
-            setTimeout(() => {
-                $(".yellow").removeClass("pressed");
-            }, 400);
-            break;
-        case 3:
-            $(".blue").addClass("pressed");
-            gameSequence[counter] = ("blue");
-            setTimeout(() => {
-                $(".blue").removeClass("pressed");
-            }, 400);
-            break;
-        default:
-            break;
-    }
-    console.log(gameSequence[counter]);
+    setTimeout(() => {
+        switch (btnNumber) {
+            case 0:
+                $(".red").addClass("pressed");
+                gameSequence[counter] = ("red");
+                setTimeout(() => {
+                    $(".red").removeClass("pressed");
+                }, 400);
+                break;
+            case 1:
+                $(".green").addClass("pressed");
+                gameSequence[counter] = ("green");
+                setTimeout(() => {
+                    $(".green").removeClass("pressed");
+                }, 400);
+                break;
+            case 2:
+                $(".yellow").addClass("pressed");
+                gameSequence[counter] = ("yellow");
+                setTimeout(() => {
+                    $(".yellow").removeClass("pressed");
+                }, 400);
+                break;
+            case 3:
+                $(".blue").addClass("pressed");
+                gameSequence[counter] = ("blue");
+                setTimeout(() => {
+                    $(".blue").removeClass("pressed");
+                }, 400);
+                break;
+            default:
+                break;
+        }
+        console.log("gameSequence losowanie:" + gameSequence);
+    }, 500);
+
 }
 
 
